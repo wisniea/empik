4dd1be532875aed926036f51f0ea0437ac1474d7
wisniea <masterflip9999@gmail.com>
2023-09-21 19:30:09 +0200

    Init commit

---
+++ src/main/java/pl/wisniea/empik/EmpikApplication.java
@@ -0,0 +1,13 @@
+package pl.wisniea.empik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empik1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Empik1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wisniea/empik/EmpikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wisniea.empik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Empik1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
